1a525952cd4b2561c1d5757a44e10c823236d792
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-03-22 16:27:58 +0300

    Added dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
+import { DashboardComponent } from './core/components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
