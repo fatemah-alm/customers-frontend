a9016e4791722b8ec77585d5303990b5da639aad
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-03-24 17:41:48 +0300

    bug fix

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('authToken');
   if (token) {
     const clonedReq = req.clone({
       setHeaders: {
